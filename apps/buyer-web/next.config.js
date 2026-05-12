07fbf3447883d7535c2bf7cec89d4c411d57a0b7
ExpertosTI <44707155+ExpertosTI@users.noreply.github.com>
2026-05-12 17:40:37 -0400

    numero333

--- apps/buyer-web/next.config.js
+++ apps/buyer-web/next.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: 'standalone', 
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
