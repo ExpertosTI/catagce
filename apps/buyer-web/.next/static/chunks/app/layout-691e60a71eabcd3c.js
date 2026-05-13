efc03b13f2cec600ef2eaedd32ed3ca9a29b43b4
ExpertosTI <44707155+ExpertosTI@users.noreply.github.com>
2026-05-13 18:19:08 -0400

    🚀 Robust Deploy: Structural Fixes & Optimized Deps

--- apps/buyer-web/.next/static/chunks/app/layout-691e60a71eabcd3c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4706:function(n,e,u){Promise.resolve().then(u.t.bind(u,2446,23))},2446:function(){}},function(n){n.O(0,[224,593,434,744],function(){return n(n.s=4706)}),_N_E=n.O()}]);
